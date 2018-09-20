757bdc0e9564981a88ef50b65ebaa0221ea2cc43
Trevor Bača <trevor.baca@gmail.com>
2018-09-20 15:02:41 -0500

    Added metronome marks.

---
+++ naehte/materials/metronome_marks/definition.py
@@ -0,0 +1,17 @@
+import abjad
+
+
+metronome_marks = abjad.OrderedDict([
+    (
+        'largo',
+        abjad.MetronomeMark((1, 4), 52),
+        ),
+    (
+        'andante',
+        abjad.MetronomeMark((1, 4), 91),
+        ),
+    (
+        'allegro',
+        abjad.MetronomeMark((1, 4), 117),
+        ),
+    ])
