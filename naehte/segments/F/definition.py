62d63573d142c35dfc0b9f5743ef2b7ff30225b7
Trevor Bača <trevor.baca@gmail.com>
2018-09-24 18:18:42 -0500

    F.

--- naehte/segments/F/definition.py
+++ naehte/segments/F/definition.py
@@ -68,3 +68,34 @@ maker(
         r"{ c'8 [ c' c' c' \times 4/5 { c' c' c' c' c' ] } }",
         ),
     )
+
+maker(
+    ('vc', 2),
+    baca.finger_pressure_transition(
+        selector=baca.leaves()[:1].lleak(),
+        ),
+    baca.rhythm(
+        r"\times 6/7 { \times 4/5 { c'4 c' c' c' c' } c' c' c' }",
+        ),
+    baca.suite(
+        baca.pitches(
+            '<B3 F#4> <E4 B4> <Ab3 Eb4> <D4 A4> <F3 C4>'
+            ' <Bb3 F4> <E3 B3> <G3 D4>',
+            allow_repeats=True,
+            ),
+        baca.glissando(),
+        ),
+    baca.text_spanner(
+        'II / III mod. =|',
+        abjad.tweak(8).staff_padding,
+        bookend=False,
+        lilypond_id=1,
+        selector=baca.leaves()[-3:].rleak(),
+        ),
+    baca.text_spanner(
+        'XFB =|',
+        abjad.tweak(5.5).staff_padding,
+        bookend=False,
+        selector=baca.leaves().rleak(),
+        ),
+    )
