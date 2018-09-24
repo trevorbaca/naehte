76d2c6e42f40d9dff83d484bd8a4f91721a26462
Trevor Bača <trevor.baca@gmail.com>
2018-09-24 08:20:19 -0500

    D.

---
+++ naehte/segments/D/__metadata__.py
@@ -0,0 +1,93 @@
+import abjad
+
+
+metadata = abjad.OrderedDict(
+    [
+        (
+            'alive_during_segment',
+            [
+                'Score',
+                'Global_Context',
+                'Global_Rests',
+                'Global_Skips',
+                'Music_Context',
+                'Cello_Music_Staff',
+                'Cello_Music_Voice',
+                'Cello_Rest_Voice',
+                ],
+            ),
+        (
+            'bol_measure_numbers',
+            [24, 27, 28],
+            ),
+        (
+            'fermata_measure_numbers',
+            [23],
+            ),
+        ('first_measure_number', 24),
+        ('last_measure_is_fermata', True),
+        ('last_measure_number', 28),
+        (
+            'persistent_indicators',
+            abjad.OrderedDict(
+                [
+                    (
+                        'Cello_Music_Staff',
+                        [
+                            abjad.Momento(
+                                context='Cello_Music_Voice',
+                                manifest='instruments',
+                                value='Cello',
+                                ),
+                            abjad.Momento(
+                                context='Cello_Music_Voice',
+                                prototype='abjad.Clef',
+                                value='bass',
+                                ),
+                            ],
+                        ),
+                    (
+                        'Cello_Music_Voice',
+                        [
+                            abjad.Momento(
+                                context='Cello_Music_Voice',
+                                prototype='abjad.Dynamic',
+                                value='niente',
+                                ),
+                            ],
+                        ),
+                    (
+                        'Score',
+                        [
+                            abjad.Momento(
+                                context='Global_Skips',
+                                manifest='metronome_marks',
+                                value='52',
+                                ),
+                            abjad.Momento(
+                                context='Global_Skips',
+                                prototype='abjad.TimeSignature',
+                                value='9/4',
+                                ),
+                            ],
+                        ),
+                    ]
+                ),
+            ),
+        ('segment_name', 'D'),
+        ('segment_number', 4),
+        (
+            'sounds_during_segment',
+            abjad.OrderedDict(
+                [
+                    ('Cello_Music_Voice', True),
+                    ('Cello_Rest_Voice', False),
+                    ]
+                ),
+            ),
+        (
+            'time_signatures',
+            ['5/8', '5/8', '5/8', '8/4', '9/4'],
+            ),
+        ]
+    )
