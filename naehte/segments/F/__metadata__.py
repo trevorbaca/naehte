addcc80a807fd1812b5535224cc9c4d466af6b49
Trevor Bača <trevor.baca@gmail.com>
2018-09-24 17:48:41 -0500

    F.

---
+++ naehte/segments/F/__metadata__.py
@@ -0,0 +1,91 @@
+import abjad
+
+
+metadata = abjad.OrderedDict(
+    [
+        (
+            'alive_during_segment',
+            [
+                'Score',
+                'Global_Context',
+                'Global_Rests',
+                'Global_Skips',
+                'Music_Context',
+                'Cello_Music_Staff',
+                'Cello_Music_Voice',
+                ],
+            ),
+        (
+            'bol_measure_numbers',
+            [35, 37],
+            ),
+        (
+            'fermata_measure_numbers',
+            [39],
+            ),
+        ('first_measure_number', 35),
+        ('last_measure_is_fermata', True),
+        ('last_measure_number', 39),
+        (
+            'persistent_indicators',
+            abjad.OrderedDict(
+                [
+                    (
+                        'Cello_Music_Staff',
+                        [
+                            abjad.Momento(
+                                context='Cello_Music_Voice',
+                                manifest='instruments',
+                                value='Cello',
+                                ),
+                            abjad.Momento(
+                                context='Cello_Music_Voice',
+                                prototype='abjad.Clef',
+                                value='bass',
+                                ),
+                            ],
+                        ),
+                    (
+                        'Cello_Music_Voice',
+                        [
+                            abjad.Momento(
+                                context='Cello_Music_Voice',
+                                prototype='abjad.Dynamic',
+                                value='p',
+                                ),
+                            ],
+                        ),
+                    (
+                        'Score',
+                        [
+                            abjad.Momento(
+                                context='Global_Skips',
+                                manifest='metronome_marks',
+                                value='117',
+                                ),
+                            abjad.Momento(
+                                context='Global_Skips',
+                                prototype='abjad.TimeSignature',
+                                value='1/4',
+                                ),
+                            ],
+                        ),
+                    ]
+                ),
+            ),
+        ('segment_name', 'F'),
+        ('segment_number', 6),
+        (
+            'sounds_during_segment',
+            abjad.OrderedDict(
+                [
+                    ('Cello_Music_Voice', False),
+                    ]
+                ),
+            ),
+        (
+            'time_signatures',
+            ['4/4', '6/4', '3/8', '5/8', '1/4'],
+            ),
+        ]
+    )
---
+++ naehte/segments/F/layout.py
@@ -0,0 +1,22 @@
+import baca
+
+
+breaks = baca.breaks(
+    baca.page(
+        baca.system(
+            16,
+            measure=1,
+            y_offset=40,
+            ),
+        baca.system(
+            16,
+            measure=3,
+            y_offset=40 + 30 + 16,
+            ),
+        ),
+    )
+spacing = baca.scorewide_spacing(
+    __file__,
+    breaks=breaks,
+    fallback_duration=(1, 32),
+    )
