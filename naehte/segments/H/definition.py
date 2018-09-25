095ff4e74cf4bf4376d1f4ea215341751a21fef7
Trevor Bača <trevor.baca@gmail.com>
2018-09-25 17:19:08 -0500

    H.

--- naehte/segments/H/definition.py
+++ naehte/segments/H/definition.py
@@ -30,3 +30,54 @@ maker(
         abjad.tweak((0, 12)).extra_offset,
         ),
     )
+
+maker(
+    ('vc', 1),
+    baca.hairpin(
+        'ppp > pppp < ppp',
+        piece_selector=baca.lparts([1, 2]),
+        ),
+    baca.note_head_style_harmonic_black(),
+    baca.stem_tremolo(
+        selector=baca.leaves(),
+        ),
+    baca.suite(
+        baca.rhythm("{ c'1 c'1 c'4 }"),
+        baca.pitches('D2 <Db2 A2> D2'),
+        baca.glissando(),
+        ),
+    baca.text_spanner(
+        'I / II larg. =|',
+        abjad.tweak(3).staff_padding,
+        bookend=False,
+        selector=baca.leaves()[-2:],
+        ),
+    )
+
+maker(
+    ('vc', 2),
+    baca.hairpin(
+        'ppp > pppp < ppp',
+        piece_selector=baca.lparts([1, 2]),
+        ),
+    baca.note_head_style_harmonic_black(),
+    baca.stem_tremolo(
+        selector=baca.leaves(),
+        ),
+    baca.suite(
+        baca.rhythm("{ c'1 c'1 c'4 }"),
+        baca.pitches('Db2 C2 Db2'),
+        baca.glissando(),
+        ),
+    baca.text_spanner(
+        '½ clt =|',
+        abjad.tweak(3).staff_padding,
+        bookend=False,
+        selector=baca.leaves()[-2:],
+        ),
+    )
+
+maker(
+    'vc',
+    baca.dls_staff_padding(6),
+    )
