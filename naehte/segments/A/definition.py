615955921805f57f0ad0f2ec057f2a149b5eeb31
Trevor Bača <trevor.baca@gmail.com>
2020-07-08 16:40:07 -0400

    Cleaned up path handling.

--- naehte/segments/A/definition.py
+++ naehte/segments/A/definition.py
@@ -1,5 +1,3 @@
-import os
-
 import abjad
 import baca
 
@@ -12,7 +10,7 @@ maker = baca.SegmentMaker(
         abjad.tags.LOCAL_MEASURE_NUMBER,
     ],
     check_all_are_pitched=True,
-    segment_directory=abjad.Path(os.path.realpath(__file__)).parent,
+    segment_directory=abjad.Path(__file__).resolve().parent,
     time_signatures=[
         (7, 8),
         (6, 8),
--- naehte/segments/H/definition.py
+++ naehte/segments/H/definition.py
@@ -1,5 +1,3 @@
-import os
-
 import abjad
 import baca
 
@@ -13,7 +11,7 @@ maker = baca.SegmentMaker(
     ],
     check_all_are_pitched=True,
     final_segment=True,
-    segment_directory=abjad.Path(os.path.realpath(__file__)).parent,
+    segment_directory=abjad.Path(__file__).resolve().parent,
     time_signatures=[
         (9, 4), (9, 4),
         (5, 8),
