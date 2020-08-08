e1936cf87013b4c8ce66da28147d43cc1d000736
Trevor Bača <trevor.baca@gmail.com>
2020-08-08 16:00:26 -0400

    Cleaned up tags.

--- naehte/segments/H/definition.py
+++ naehte/segments/H/definition.py
@@ -7,7 +7,7 @@ import baca
 
 maker = baca.SegmentMaker(
     activate=[
-        abjad.tags.LOCAL_MEASURE_NUMBER,
+        baca.tags.LOCAL_MEASURE_NUMBER,
     ],
     check_all_are_pitched=True,
     final_segment=True,
