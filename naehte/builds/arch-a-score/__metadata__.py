f52feb947f04e90d664c6993abaf87d93331b262
Trevor Bača <trevor.baca@gmail.com>
2020-08-26 14:56:51 -0400

    Blacked metadata files.

--- naehte/builds/arch-a-score/__metadata__.py
+++ naehte/builds/arch-a-score/__metadata__.py
@@ -4,7 +4,7 @@ import abjad
 metadata = abjad.OrderedDict(
     [
         (
-            'bol_measure_numbers',
+            "bol_measure_numbers",
             [
                 1,
                 4,
@@ -38,9 +38,9 @@ metadata = abjad.OrderedDict(
                 103,
                 105,
                 110,
-                ],
-            ),
-        ('paper_size', 'arch a'),
-        ('price', '$28'),
-        ]
-    )
+            ],
+        ),
+        ("paper_size", "arch a"),
+        ("price", "$28"),
+    ]
+)
