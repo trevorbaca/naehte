d746518cf222abbec0c0056bfe30a386ff530fbc
Trevor Bača <trevor.baca@gmail.com>
2020-08-26 14:58:01 -0400

    Blacked metadata files.

--- naehte/segments/A/__metadata__.py
+++ naehte/segments/A/__metadata__.py
@@ -4,28 +4,28 @@ import abjad
 metadata = abjad.OrderedDict(
     [
         (
-            'bol_measure_numbers',
+            "bol_measure_numbers",
             [1, 4, 7],
-            ),
-        ('duration', "0'15''"),
-        ('final_measure_number', 8),
-        ('first_measure_number', 1),
-        ('segment_name', 'A'),
-        ('segment_number', 1),
-        ('start_clock_time', "0'00''"),
-        ('stop_clock_time', "0'15''"),
+        ),
+        ("duration", "0'15''"),
+        ("final_measure_number", 8),
+        ("first_measure_number", 1),
+        ("segment_name", "A"),
+        ("segment_number", 1),
+        ("start_clock_time", "0'00''"),
+        ("stop_clock_time", "0'15''"),
         (
-            'time_signatures',
+            "time_signatures",
             [
-                '7/8',
-                '6/8',
-                '5/4',
-                '6/8',
-                '5/4',
-                '4/4',
-                '2/4',
-                '4/4',
-                ],
-            ),
-        ]
-    )
+                "7/8",
+                "6/8",
+                "5/4",
+                "6/8",
+                "5/4",
+                "4/4",
+                "2/4",
+                "4/4",
+            ],
+        ),
+    ]
+)
