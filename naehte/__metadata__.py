768cd73d67eab1f9c6ae92b3b2b17e615c89cb66
Trevor Bača <trevor.baca@gmail.com>
2021-07-10 15:26:55 -0400

    Cleaned up fermata measure number metadata.

--- naehte/__metadata__.py
+++ naehte/__metadata__.py
@@ -4,41 +4,6 @@ metadata = abjad.OrderedDict(
     [
         ("catalog_number", "AWN-018"),
         ("composer", "Bača"),
-        (
-            "fermata_measure_numbers",
-            abjad.OrderedDict(
-                [
-                    (
-                        "B",
-                        [14, 16, 18],
-                    ),
-                    (
-                        "C",
-                        [34, 39, 45],
-                    ),
-                    (
-                        "D",
-                        [55],
-                    ),
-                    (
-                        "E",
-                        [72],
-                    ),
-                    (
-                        "F",
-                        [81, 86, 88],
-                    ),
-                    (
-                        "G",
-                        [102],
-                    ),
-                    (
-                        "H",
-                        [106, 111, 116],
-                    ),
-                ]
-            ),
-        ),
         ("forces_tagline", "for cello"),
         ("title", "Nähte"),
         ("year", 2018),
