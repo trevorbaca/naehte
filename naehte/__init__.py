2f7059489b4e9f38ae76fe774e26dc59795e701a
Trevor Bača <trevor.baca@gmail.com>
2021-07-28 15:50:18 -0400

    Taught package initializer to check for Python 3.9 or later.

--- naehte/__init__.py
+++ naehte/__init__.py
@@ -1,5 +1,4 @@
-import distutils.version
-import platform
+import sys
 
 from .library import ScoreTemplate, instruments, metronome_marks
 
@@ -9,10 +8,6 @@ __all__ = [
     "metronome_marks",
 ]
 
-if not (
-    distutils.version.LooseVersion("3.7")
-    < distutils.version.LooseVersion(platform.python_version())
-):
-    raise ImportError("Requires Python 3.7.")
-del distutils
-del platform
+if sys.version_info[:2] < (3, 9):
+    raise ImportError("Requires Python 3.9 or later")
+del sys
