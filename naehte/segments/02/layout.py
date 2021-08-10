144574e67fb86c61adfb6c3c048a569c004c2327
Trevor Bača <trevor.baca@gmail.com>
2021-08-09 20:36:17 -0400

    Cleaned up layout files.

--- naehte/segments/02/layout.py
+++ naehte/segments/02/layout.py
@@ -12,20 +12,18 @@ breaks = baca.breaks(
     ),
 )
 
-spacing = baca.spacing(
-    breaks=breaks,
-    fallback_duration=(1, 32),
-    overrides=(
-        baca.space(1, (1, 56)),
-        baca.space(2, (1, 56)),
-        baca.space(3, (1, 48)),
-        baca.space(5, (1, 48)),
-        baca.space(7, (1, 48)),
-        baca.space(9, (1, 24)),
-        baca.space(14, (1, 56)),
-        baca.space((16, 19), (1, 14)),
-    ),
-)
-
 if __name__ == "__main__":
-    baca.build.make_layout_ly(breaks, spacing)
+    baca.build.make_layout_ly(
+        breaks,
+        fallback_duration=(1, 32),
+        overrides=(
+            baca.space(1, (1, 56)),
+            baca.space(2, (1, 56)),
+            baca.space(3, (1, 48)),
+            baca.space(5, (1, 48)),
+            baca.space(7, (1, 48)),
+            baca.space(9, (1, 24)),
+            baca.space(14, (1, 56)),
+            baca.space((16, 19), (1, 14)),
+        ),
+    )
