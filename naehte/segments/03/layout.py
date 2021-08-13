03b7f0bb67557121af4b0b6f56695ad197d6bcef
Trevor Bača <trevor.baca@gmail.com>
2021-08-13 11:35:45 -0400

    Cleaned up layout files.

--- naehte/segments/03/layout.py
+++ naehte/segments/03/layout.py
@@ -2,26 +2,26 @@ import baca
 
 distances = (16,)
 
-breaks = baca.breaks(
-    baca.page(
-        baca.system(measure=1, y_offset=30 + 0 * (35 + 16), distances=distances),
-        baca.system(measure=4, y_offset=30 + 1 * (35 + 16), distances=distances),
-        baca.system(measure=9, y_offset=30 + 2 * (35 + 16), distances=distances),
-        baca.system(measure=13, y_offset=30 + 3 * (35 + 16), distances=distances),
-        baca.system(measure=16, y_offset=30 + 4 * (35 + 16), distances=distances),
+spacing = baca.SpacingSpecifier(
+    breaks=baca.breaks(
+        baca.page(
+            baca.system(measure=1, y_offset=30 + 0 * (35 + 16), distances=distances),
+            baca.system(measure=4, y_offset=30 + 1 * (35 + 16), distances=distances),
+            baca.system(measure=9, y_offset=30 + 2 * (35 + 16), distances=distances),
+            baca.system(measure=13, y_offset=30 + 3 * (35 + 16), distances=distances),
+            baca.system(measure=16, y_offset=30 + 4 * (35 + 16), distances=distances),
+        ),
+    ),
+    fallback_duration=(1, 32),
+    overrides=(
+        baca.space(4, (1, 12)),
+        baca.space(5, (1, 16)),
+        baca.space(8, (1, 12)),
+        baca.space((9, 10), (1, 48)),
+        baca.space(13, (1, 40)),
+        baca.space(15, (1, 24)),
     ),
 )
 
 if __name__ == "__main__":
-    baca.build.make_layout_ly(
-        breaks,
-        fallback_duration=(1, 32),
-        overrides=(
-            baca.space(4, (1, 12)),
-            baca.space(5, (1, 16)),
-            baca.space(8, (1, 12)),
-            baca.space((9, 10), (1, 48)),
-            baca.space(13, (1, 40)),
-            baca.space(15, (1, 24)),
-        ),
-    )
+    baca.build.make_layout_ly(spacing)
--- naehte/segments/04/layout.py
+++ naehte/segments/04/layout.py
@@ -2,25 +2,25 @@ import baca
 
 distances = (16,)
 
-breaks = baca.breaks(
-    baca.page(
-        baca.system(measure=1, y_offset=30 + 0 * (35 + 16), distances=distances),
-        baca.system(measure=4, y_offset=30 + 1 * (35 + 16), distances=distances),
-        baca.system(measure=8, y_offset=30 + 2 * (35 + 16), distances=distances),
-        baca.system(measure=12, y_offset=30 + 3 * (35 + 16), distances=distances),
-        baca.system(measure=15, y_offset=30 + 4 * (35 + 16), distances=distances),
+spacing = baca.SpacingSpecifier(
+    breaks=baca.breaks(
+        baca.page(
+            baca.system(measure=1, y_offset=30 + 0 * (35 + 16), distances=distances),
+            baca.system(measure=4, y_offset=30 + 1 * (35 + 16), distances=distances),
+            baca.system(measure=8, y_offset=30 + 2 * (35 + 16), distances=distances),
+            baca.system(measure=12, y_offset=30 + 3 * (35 + 16), distances=distances),
+            baca.system(measure=15, y_offset=30 + 4 * (35 + 16), distances=distances),
+        ),
+    ),
+    fallback_duration=(1, 32),
+    overrides=(
+        baca.space(5, (1, 56)),
+        baca.space(6, (1, 24)),
+        baca.space(7, (1, 48)),
+        baca.space((13, 14), (1, 12)),
+        baca.space((15, 16), (1, 24)),
     ),
 )
 
 if __name__ == "__main__":
-    baca.build.make_layout_ly(
-        breaks,
-        fallback_duration=(1, 32),
-        overrides=(
-            baca.space(5, (1, 56)),
-            baca.space(6, (1, 24)),
-            baca.space(7, (1, 48)),
-            baca.space((13, 14), (1, 12)),
-            baca.space((15, 16), (1, 24)),
-        ),
-    )
+    baca.build.make_layout_ly(spacing)
