c2892d4acac144157062c493f1e86813623082ee
Trevor Bača <trevor.baca@gmail.com>
2021-09-29 10:10:53 +0200

    Cleaned up empty score construction.

--- naehte/segments/07/music.py
+++ naehte/segments/07/music.py
@@ -3,15 +3,17 @@ import baca
 
 from naehte import library as naehte
 
-###############################################################################
-##################################### [G] #####################################
-###############################################################################
+#########################################################################################
+########################################### 07 ##########################################
+#########################################################################################
+
+score = naehte.make_empty_score()
+voice_names = baca.accumulator.get_voice_names(score)
 
 commands = baca.CommandAccumulator(
     **baca.segment_accumulation_defaults(),
     instruments=naehte.instruments,
     metronome_marks=naehte.metronome_marks,
-    score_template=naehte.make_empty_score,
     time_signatures=[
         (4, 4),
         (5, 4),
@@ -29,6 +31,7 @@ commands = baca.CommandAccumulator(
         (1, 4),
     ],
     voice_abbreviations=naehte.voice_abbreviations,
+    voice_names=voice_names,
 )
 
 commands(
@@ -481,4 +484,5 @@ if __name__ == "__main__":
         do_not_require_margin_markup=True,
         global_rests_in_topmost_staff=True,
         error_on_not_yet_pitched=True,
+        score=score,
     )
