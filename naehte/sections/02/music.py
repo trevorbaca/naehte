4ed665301d6ac3189b502530ba89878e6b6feacc
Trevor Bača <trevor.baca@gmail.com>
2022-05-13 16:01:23 +0200

    Cleaned up  music.py files.

--- naehte/sections/02/music.py
+++ naehte/sections/02/music.py
@@ -708,18 +708,15 @@ commands(
 )
 
 if __name__ == "__main__":
-    metadata, persist, score, timing = baca.build.interpret_segment(
+    metadata, persist, score, timing = baca.build.interpret_section(
         score,
         commands,
         **baca.score_interpretation_defaults(),
         activate=(baca.tags.LOCAL_MEASURE_NUMBER,),
         always_make_global_rests=True,
-        append_phantom_measures_by_hand=True,
         do_not_require_margin_markup=True,
-        do_not_sort_commands=True,
         error_on_not_yet_pitched=True,
         global_rests_in_topmost_staff=True,
-        intercalate_mmrests_by_hand=True,
     )
     lilypond_file = baca.make_lilypond_file(
         score,
