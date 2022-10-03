a63fc8cfb7f275814f792dce27d1eefe8389b29b
Trevor Bača <trevor.baca@gmail.com>
2022-10-03 06:18:00 -0400

    Cleaned up timing.

--- naehte/sections/08/music.py
+++ naehte/sections/08/music.py
@@ -292,11 +292,10 @@ def make_score(first_measure_number, previous_persistent_indicators):
 
 def main():
     environment = baca.build.read_environment(__file__, baca.build.argv())
-    timing = baca.build.Timing()
     score, measures = make_score(
         environment.first_measure_number,
         environment.previous_persist["persistent_indicators"],
-        timing,
+        environment.timing,
     )
     defaults = baca.section.section_defaults()
     del defaults["append_anchor_skip"]
@@ -312,7 +311,6 @@ def main():
         final_section=True,
         global_rests_in_topmost_staff=True,
         manifests=library.manifests,
-        timing=timing,
     )
     lilypond_file = baca.lilypond.file(
         score,
@@ -323,7 +321,7 @@ def main():
         lilypond_file,
         metadata,
         persist,
-        timing,
+        environment.timing,
         environment.arguments,
     )
 
