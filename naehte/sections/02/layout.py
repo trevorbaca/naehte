02ec89c1d93c20067f6d4ff398e4601128d1c54b
Trevor Bača <trevor.baca@gmail.com>
2023-09-08 13:16:00 -0400

    Cleaned up layout.py files.

--- naehte/sections/02/layout.py
+++ naehte/sections/02/layout.py
@@ -2,27 +2,31 @@ import baca
 
 distances = (16,)
 
-spacing = baca.make_layout(
-    baca.page(
-        1,
-        baca.system(measure=1, y_offset=30 + 0 * (35 + 16), distances=distances),
-        baca.system(measure=4, y_offset=30 + 1 * (35 + 16), distances=distances),
-        baca.system(measure=9, y_offset=30 + 2 * (35 + 16), distances=distances),
-        baca.system(measure=12, y_offset=30 + 3 * (35 + 16), distances=distances),
-        baca.system(measure=16, y_offset=30 + 4 * (35 + 16), distances=distances),
-    ),
-    spacing=(1, 32),
-    overrides=(
-        baca.space(1, (1, 56)),
-        baca.space(2, (1, 56)),
-        baca.space(3, (1, 48)),
-        baca.space(5, (1, 48)),
-        baca.space(7, (1, 48)),
-        baca.space(9, (1, 24)),
-        baca.space(14, (1, 56)),
-        baca.space((16, 19), (1, 14)),
-    ),
-)
+
+def main():
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=30 + 0 * (35 + 16), distances=distances),
+            baca.system(measure=4, y_offset=30 + 1 * (35 + 16), distances=distances),
+            baca.system(measure=9, y_offset=30 + 2 * (35 + 16), distances=distances),
+            baca.system(measure=12, y_offset=30 + 3 * (35 + 16), distances=distances),
+            baca.system(measure=16, y_offset=30 + 4 * (35 + 16), distances=distances),
+        ),
+        spacing=(1, 32),
+        overrides=(
+            baca.space(1, (1, 56)),
+            baca.space(2, (1, 56)),
+            baca.space(3, (1, 48)),
+            baca.space(5, (1, 48)),
+            baca.space(7, (1, 48)),
+            baca.space(9, (1, 24)),
+            baca.space(14, (1, 56)),
+            baca.space((16, 19), (1, 14)),
+        ),
+    )
+    baca.section.make_layout_ly(spacing)
+
 
 if __name__ == "__main__":
-    baca.section.make_layout_ly(spacing)
+    main()
