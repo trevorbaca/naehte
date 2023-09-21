5cf14a5d4ccd4deae7d34979dde716497eeee532
Trevor Bača <trevor.baca@gmail.com>
2023-09-21 12:42:55 -0400

    Cleaned up music.py files.

--- naehte/sections/03/music.py
+++ naehte/sections/03/music.py
@@ -618,5 +618,29 @@ def main():
     )
 
 
+def make_layout():
+    distances = (16,)
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=30 + 0 * (35 + 16), distances=distances),
+            baca.system(measure=4, y_offset=30 + 1 * (35 + 16), distances=distances),
+            baca.system(measure=9, y_offset=30 + 2 * (35 + 16), distances=distances),
+            baca.system(measure=13, y_offset=30 + 3 * (35 + 16), distances=distances),
+            baca.system(measure=16, y_offset=30 + 4 * (35 + 16), distances=distances),
+        ),
+        spacing=(1, 32),
+        overrides=(
+            baca.space(4, (1, 12)),
+            baca.space(5, (1, 16)),
+            baca.space(8, (1, 12)),
+            baca.space((9, 10), (1, 48)),
+            baca.space(13, (1, 40)),
+            baca.space(15, (1, 24)),
+        ),
+    )
+    baca.section.make_layout_ly(spacing)
+
+
 if __name__ == "__main__":
     main()
--- naehte/sections/04/music.py
+++ naehte/sections/04/music.py
@@ -600,5 +600,28 @@ def main():
     )
 
 
+def make_layout():
+    distances = (16,)
+    spacing = baca.make_layout(
+        baca.page(
+            1,
+            baca.system(measure=1, y_offset=30 + 0 * (35 + 16), distances=distances),
+            baca.system(measure=4, y_offset=30 + 1 * (35 + 16), distances=distances),
+            baca.system(measure=8, y_offset=30 + 2 * (35 + 16), distances=distances),
+            baca.system(measure=12, y_offset=30 + 3 * (35 + 16), distances=distances),
+            baca.system(measure=15, y_offset=30 + 4 * (35 + 16), distances=distances),
+        ),
+        spacing=(1, 32),
+        overrides=(
+            baca.space(5, (1, 56)),
+            baca.space(6, (1, 24)),
+            baca.space(7, (1, 48)),
+            baca.space((13, 14), (1, 12)),
+            baca.space((15, 16), (1, 24)),
+        ),
+    )
+    baca.section.make_layout_ly(spacing)
+
+
 if __name__ == "__main__":
     main()
