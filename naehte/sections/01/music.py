5c410acbcfcf1b94e74b97a6ae772b031ffc362d
Trevor Bača <trevor.baca@gmail.com>
2024-04-19 15:37:05 -0400

    Cleaned up music.py files.

--- naehte/sections/01/music.py
+++ naehte/sections/01/music.py
@@ -269,7 +269,7 @@ def make_score():
 
 
 def persist_score(score, environment):
-    metadata = baca.section.postprocess_score(
+    metadata = baca.section.postprocess(
         score,
         environment,
         library.manifests,
